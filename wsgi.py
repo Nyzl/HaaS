f73e33f4d67d3b678ce7699fd9b994fa34738848
Ian Ansell <ian.ansell.uk@gmail.com>
2022-07-08 17:11:50 +0100

    add debugging

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@ import os
 from app import app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
