8571b7e642ec35242cf826fb062e0c799db3d065
Andy Driver <andy@pagezero.net>
2023-01-24 13:53:28 +0000

    Bump Consent API Client to 1.5.0
    * Add Consent API URL to data-attribute
    * Reorder JS files - Consent API client needs to be loaded first

--- haas/assets.py
+++ haas/assets.py
@@ -4,9 +4,9 @@ from webassets.filter import get_filter
 
 cookies_js = Bundle(
     "javascripts/utils.js",
+    "javascripts/consent.js",
     "javascripts/cookie-banner.js",
     "javascripts/cookies-page.js",
-    "javascripts/consent.js",
     filters=get_filter(
         "babel",
         binary="node_modules/@babel/cli/bin/babel.js",
--- makefile
+++ makefile
@@ -44,7 +44,7 @@ assets: consent_api
 
 .PHONY: run
 run:
-	flask --debug run --debugger --reload --port $(PORT)
+	flask --debug run --debugger --reload --host "0.0.0.0" --port $(PORT)
 
 .PHONY: docker-image
 docker-image: assets
