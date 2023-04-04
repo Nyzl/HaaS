5cba516c160190a523c96800c02b8d411ac0bf83
Ian Ansell <ian.ansell.uk@gmail.com>
2023-04-04 09:49:40 +0100

    move GTM block to base.html (#4)
    move GTM block to base.html and default to GA4 tracking

--- haas/views.py
+++ haas/views.py
@@ -7,10 +7,21 @@ from haas import app
 from haas.generator import generate_hex
 
 
+container_id = {
+        "ua": "GTM-5S9XWS6",
+        "ga4": "GTM-NBCMVLT",
+    }
+
 @app.get("/")
 def home():
     """Show home page."""
-    return render_template("index.html", title="Home")
+    ga_version = request.args.get("type", default="ga4")
+    return render_template(
+        "index.html", 
+        container_id=container_id[ga_version], 
+        title="Home",
+        type=ga_version,
+    )
 
 
 @app.get("/api")
@@ -23,10 +34,6 @@ def api():
 def analytics():
     """Render a hexagram reading with the submitted number of hexagrams."""
     ga_version = request.args.get("type", default="ga4")
-    container_id = {
-        "ua": "GTM-5S9XWS6",
-        "ga4": "GTM-NBCMVLT",
-    }[ga_version]
 
     if request.method == "POST":
         input = request.form["hex_count"] or "6"
@@ -36,13 +43,19 @@ def analytics():
 
     return render_template(
         "reading.html",
-        container_id=container_id,
+        container_id=container_id[ga_version],
         title="Hexagrams",
         hexagrams=generate_hex(num),
+        type=ga_version,
     )
 
 
 @app.get("/cookies")
 def cookies():
     """Render the cookies page."""
-    return render_template("cookies.html")
+    ga_version = request.args.get("type", default="ga4")
+    return render_template(
+        "cookies.html", 
+        container_id=container_id[ga_version],
+        type=ga_version,
+    )
